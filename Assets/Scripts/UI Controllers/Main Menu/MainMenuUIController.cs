d2d0f8bedacbc1a822065044fef4e3dcbcad2f8a
Dannan Vazquez <dannanvazquez@gmail.com>
2023-09-18 00:14:29 -0700

    Quit Button Conditional Compilation
    Added conditional compilations to the quit button so it works in both editor and WebGL.

--- Assets/Scripts/UI Controllers/Main Menu/MainMenuUIController.cs
+++ Assets/Scripts/UI Controllers/Main Menu/MainMenuUIController.cs
@@ -117,6 +117,12 @@ public class MainMenuUIController : MonoBehaviour {
     }
 
     public void QuitApplication() {
+#if (UNITY_EDITOR)
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif (UNITY_STANDALONE)
         Application.Quit();
+#elif (UNITY_WEBGL)
+        Application.OpenURL(Application.absoluteURL);
+#endif
     }
 }
